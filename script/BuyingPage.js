8c3baf779c0091ecb2218a681528e3d767b6bb99
jiyeonCoder <jiyeon.choi218@gmail.com>
2023-11-29 02:34:39 +0900

    Feat(payment): 전체 상품 가격 보여줌

--- script/BuyingPage.js
+++ script/BuyingPage.js
@@ -17,28 +17,26 @@ async function cartList(data){
       }
   }).then((res)=>{
     const productList = res.data;
-
     const productListContainer = $("#productListContainer");
+    let totalPriceEntire = 0
 
     productList.forEach((product, index)=>{
       const productDiv = $("<div>").css({"display": "flex"});
-
       const productNameLi = $("<li>").text(product.product_name).addClass("products").attr("id", `product_name_${index + 1}`);
-
       const quantityLi = $("<li>").addClass("products").attr("id", `quantity_${index + 1}`).append($("<div>").addClass("form-group").append($("<label>").text("Quantity: "), $("<input>").attr("type", "text").addClass("form-control quantityInput")));
 
       const totalPriceProduct = product.price * product.quantity;
-      const totalPriceEntire = totalPriceProduct
+      totalPriceEntire += totalPriceProduct;
       
       const priceLi = $("<li>").text(`${totalPriceProduct}원`).addClass("price").attr("id", `price_${index + 1}`);
 
       productDiv.append(productNameLi, quantityLi, priceLi);
-
       productDiv.append($("<hr>").css("width", "98%"));
-
       productListContainer.append(productDiv);
 
       $(`#quantity_${index + 1} .quantityInput`).val(product.quantity);
-    })
+    });
+
+    $("#totalPriceEntire").text(`${totalPriceEntire}원`);
   })
 }
