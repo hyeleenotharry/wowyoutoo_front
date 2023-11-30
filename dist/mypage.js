e0d97670311d57b0719c837464733decae3ce0f4
hyeleenotharry <dlgpgl1029@gmail.com>
2023-11-30 23:12:45 +0900

    Fix: css 경로 재설정

--- dist/mypage.js
+++ dist/mypage.js
@@ -1,1 +1,238 @@
-(()=>{"use strict";const e=document.createElement("link");e.rel="stylesheet",e.href="../css/myPage.css",document.head.appendChild(e);const t="http://127.0.0.1:8000";function n(e){"프로필 수정"==e.target.text?function(e){const t=document.getElementById("nickname"),n=document.getElementById("email"),o=document.getElementById("profile-img");t.style.display="none",n.style.display="none",o.style.display="none";const l=document.getElementById("new-nick"),d=document.getElementById("ex-pwd"),a=document.getElementById("new-pwd1"),i=document.getElementById("new-pwd2"),c=document.getElementById("new-img");var m,s,r;l.style.display="block",d.style.display="block",a.style.display="block",i.style.display="block",c.style.display="block",document.getElementById("modify").innerText="수정 완료",m=document.getElementById("profile-box"),s=document.getElementById("menu-box"),r=m.offsetHeight,console.log(r),m.style.marginTop="-450px",s.style.marginTop="600px"}():async function(){const e=localStorage.getItem("access"),n=JSON.parse(localStorage.getItem("payload")).user_id,o=document.getElementById("nick-val").value,l=document.getElementById("ex-pwd-val").value,d=document.getElementById("new-pwd-val1").value,a=document.getElementById("new-pwd-val2").value;let i=new FormData;try{document.getElementById("new-profile-img").files[0]&&(i.append("profile_img",document.getElementById("new-profile-img").files[0]),console.log(document.getElementById("new-profile-img").files[0]))}catch(e){console.log(e.response)}if(null==l&&alert("기존 비밀번호는 필수로 입력해주세요"),d.val!=a.val)alert("입력한 비밀번호와 비밀번호 확인이 다릅니다.");else{i.append("nickname",o),i.append("present_pw",l),i.append("password",d),i.append("password_check",a);try{await fetch(`${t}/accounts/profile/${n}/`,{headers:{Authorization:`Bearer ${e}`},method:"PUT",body:i}).then((e=>{200==e.status&&(alert("프로필 수정이 완료되었습니다."),window.location.reload())}))}catch(e){console.log(e.response.data.message)}}}()}function o(){window.location.href="../templates/myWord.html"}function l(){window.location.href="../templates/SavedReading.html"}$(document).ready((function(){$("#modify").on("click",n),$("#my-word").on("click",o),$("#my-reading").on("click",l);const e=document.getElementById("new-nick"),d=document.getElementById("ex-pwd"),a=document.getElementById("new-pwd1"),i=document.getElementById("new-pwd2"),c=document.getElementById("new-img");e.style.display="none",d.style.display="none",a.style.display="none",i.style.display="none",c.style.display="none";try{localStorage.getItem("provider")&&(document.getElementById("modify").style.display="none")}catch(e){console.log(e)}!async function(){const e=JSON.parse(localStorage.getItem("payload")).user_id,n=localStorage.getItem("access");try{await fetch(`${t}/accounts/profile/${e}/`,{headers:{Authorization:`Bearer ${n}`},method:"GET"}).then((e=>{if(200==e.status)return e.json()})).then((e=>{const n=document.getElementById("nickname"),o=document.getElementById("email"),l=(document.getElementById("profile-img"),document.getElementById("my-coin"));if("undefined"==e.profile_img)$("#profile-img").attr("src","https://media.licdn.com/dms/image/C5103AQE6wN_SiFGQrQ/profile-displayphoto-shrink_200_200/0/1517547117016?e=1701907200&v=beta&t=OIxWdliiKCDdNi-fyFFCUthcuYUcmSo6jQWTYX5uEt4");else{var d=t+e.profile_img,a=decodeURIComponent(d),i=a.indexOf("media/")+6,c=a.substring(i);console.log(c),n.innerText=e.nickname,o.innerText=e.email,l.innerText=e.coin;try{if("github"==localStorage.getItem("provider"))$("#profile-img").attr("src",c);else if("kakao"==localStorage.getItem("provider")){var m=c.replace("http:/","https://");$("#profile-img").attr("src",m)}else $("#profile-img").attr("src",d)}catch(e){console.log(e),alert(e)}}const s=document.getElementById("word-quiz"),r=document.getElementById("reading-quiz"),y=document.getElementById("rank");s.innerText=e.word_nums,r.innerText=e.reading_nums,y.innerText=`${e.my_rank} 위`,$("#nick-val").val(e.nickname)}))}catch(e){alert(e),window.location.href="../templates/main.html"}}()}))})();
+import config from '../APIkey.js'
+// import '../css/myPage.css'
+
+const linkElement = document.createElement("link");
+linkElement.rel = "stylesheet";
+linkElement.href = "../css/myPage.css"; // 여기에 CSS 파일 경로를 넣어야 해
+document.head.appendChild(linkElement); // 문서의 head에 link 요소를 추가하여 CSS를 가져옴
+
+const backend_base_url = config.backend_base_url
+const frontend_base_url = config.frontend_base_url
+
+function handlePersonalInfo(e) {
+    // console.log(e.target.text)
+    const nickname = document.getElementById("nickname")
+    const email = document.getElementById("email")
+    const profile_img = document.getElementById("profile-img")
+
+    nickname.style.display = 'none'
+    email.style.display = 'none'
+    profile_img.style.display = 'none'
+
+    const new_nick = document.getElementById("new-nick")
+    const ex_pwd = document.getElementById("ex-pwd")
+    const new_pwd1 = document.getElementById("new-pwd1")
+    const new_pwd2 = document.getElementById("new-pwd2")
+    const new_img = document.getElementById("new-img")
+
+
+    new_nick.style.display = 'block'
+    ex_pwd.style.display = 'block'
+    new_pwd1.style.display = 'block'
+    new_pwd2.style.display = 'block'
+    new_img.style.display = 'block'
+
+    const submit = document.getElementById('modify')
+
+    submit.innerText = "수정 완료"
+
+    readjustMenu()
+}
+
+async function changePersonalInfo() {
+    const access = localStorage.getItem('access')
+
+    const uid = JSON.parse(localStorage.getItem('payload'))['user_id']
+    const new_nickname = document.getElementById("nick-val").value
+    const ex_password = document.getElementById("ex-pwd-val").value
+    const new_password1 = document.getElementById("new-pwd-val1").value
+    const new_password2 = document.getElementById("new-pwd-val2").value
+
+    let formData = new FormData()
+
+    try {
+        if (document.getElementById("new-profile-img").files[0]) {
+            formData.append(
+                "profile_img",
+                document.getElementById("new-profile-img").files[0]
+            );
+            console.log(document.getElementById("new-profile-img").files[0])
+        }
+    } catch (error) {
+        console.log(error.response)
+    }
+
+    if (ex_password == undefined) {
+        alert("기존 비밀번호는 필수로 입력해주세요")
+    }
+
+    if (new_password1.val != new_password2.val) {
+        alert("입력한 비밀번호와 비밀번호 확인이 다릅니다.")
+    } else {
+        formData.append('nickname', new_nickname)
+        formData.append("present_pw", ex_password)
+
+        formData.append('password', new_password1)
+        formData.append('password_check', new_password2)
+
+
+        try {
+            const response = await fetch(`${backend_base_url}/accounts/profile/${uid}/`, {
+                headers: {
+                    Authorization: `Bearer ${access}`,
+                },
+                method: 'PUT',
+                body: formData
+
+            }).then((res) => {
+                if (res.status == 200) {
+                    alert("프로필 수정이 완료되었습니다.")
+                    window.location.reload()
+                }
+            })
+        } catch (error) {
+            console.log(error.response.data['message'])
+        }
+
+    }
+
+}
+
+function handleProfile(e) {
+    if (e.target.text == "프로필 수정") {
+        handlePersonalInfo()
+    } else {
+        changePersonalInfo()
+    }
+}
+
+function readjustMenu() {
+    var profileBox = document.getElementById('profile-box');
+    var menu = document.getElementById('menu-box');
+    var profileBoxHeight = profileBox.offsetHeight;
+    console.log(profileBoxHeight)
+    profileBox.style.marginTop = -450 + 'px'
+    menu.style.marginTop = 600 + 'px';
+}
+
+
+function gotoWord() {
+    window.location.href = "../templates/myWord.html"
+}
+
+function gotoReading() {
+    window.location.href = "../templates/SavedReading.html"
+}
+
+async function getProfile() {
+    const uid = JSON.parse(localStorage.getItem('payload'))['user_id']
+
+    const access = localStorage.getItem('access')
+    // console.log(`Bearer ${access}`)
+    try {
+        const response = await fetch(`${backend_base_url}/accounts/profile/${uid}/`, {
+            headers: {
+                'Authorization': `Bearer ${access}`
+            },
+            method: 'GET'
+        }).then((res) => {
+            // console.log(res.json())
+            if (res.status == 200) {
+                return res.json()
+            }
+        }).then((res) => {
+            // 프로필 정보
+            const nick = document.getElementById('nickname')
+            const email = document.getElementById('email')
+            const img_url = document.getElementById('profile-img')
+            const my_coin = document.getElementById('my-coin')
+
+            if (res['profile_img'] == 'undefined') {
+                $('#profile-img').attr("src", 'https://media.licdn.com/dms/image/C5103AQE6wN_SiFGQrQ/profile-displayphoto-shrink_200_200/0/1517547117016?e=1701907200&v=beta&t=OIxWdliiKCDdNi-fyFFCUthcuYUcmSo6jQWTYX5uEt4');
+            } else {
+
+                var fullURL = backend_base_url + res['profile_img']
+
+                // URL 디코딩
+                var decodedURL = decodeURIComponent(fullURL);
+
+                // "media/" 이후의 부분 추출
+                var startIndex = decodedURL.indexOf("media/") + "media/".length;
+                var extractedURL = decodedURL.substring(startIndex);
+
+                console.log(extractedURL)
+
+                nick.innerText = res['nickname']
+                email.innerText = res['email']
+                my_coin.innerText = res['coin']
+                try {
+                    if (localStorage.getItem('provider') == 'github') {
+                        $('#profile-img').attr("src", extractedURL);
+                    }
+                    else if (localStorage.getItem('provider') == 'kakao') {
+                        var trimmedURL = extractedURL.replace('http:/', 'https://');
+                        $('#profile-img').attr("src", trimmedURL);
+                    }
+                    else {
+                        $('#profile-img').attr("src", fullURL);
+                    }
+
+                } catch (error) {
+                    console.log(error)
+                    alert(error)
+                }
+            }
+
+
+            // 내 단어 , 지문, 푼 문제 수
+            const myWords = document.getElementById('word-quiz')
+            const myReadings = document.getElementById('reading-quiz')
+            const rank = document.getElementById('rank')
+
+            myWords.innerText = res['word_nums']
+            myReadings.innerText = res['reading_nums']
+            rank.innerText = `${res['my_rank']} 위`
+
+            // 새 프로필에 이전 정보
+
+            $('#nick-val').val(res['nickname'])
+
+
+        })
+    }
+    catch (error) {
+        alert(error)
+        window.location.href = '../templates/main.html'
+    }
+
+}
+
+
+$(document).ready(function () {
+    $('#modify').on('click', handleProfile)
+    $('#my-word').on('click', gotoWord)
+    $('#my-reading').on('click', gotoReading)
+
+    const new_nick = document.getElementById("new-nick")
+    const ex_pwd = document.getElementById("ex-pwd")
+    const new_pwd1 = document.getElementById("new-pwd1")
+    const new_pwd2 = document.getElementById("new-pwd2")
+    const new_img = document.getElementById("new-img")
+
+    new_nick.style.display = 'none'
+    ex_pwd.style.display = 'none'
+    new_pwd1.style.display = 'none'
+    new_pwd2.style.display = 'none'
+    new_img.style.display = 'none'
+    try {
+        if (localStorage.getItem('provider')) {
+            const profile_btn = document.getElementById('modify')
+            profile_btn.style.display = 'none'
+        }
+    } catch (error) {
+        console.log(error)
+    }
+
+    getProfile()
+
+})
