c5e4300fab25a538582572952c8c25d310886c83
hyeleenotharry <dlgpgl1029@gmail.com>
2023-12-01 01:38:46 +0900

    Fix: dist 경로 수정

--- dist/mypage.js
+++ dist/mypage.js
@@ -3,7 +3,7 @@ import config from '../APIkey.js'
 
 const linkElement = document.createElement("link");
 linkElement.rel = "stylesheet";
-linkElement.href = "../css/myPage.css"; // 여기에 CSS 파일 경로를 넣어야 해
+linkElement.href = "myPage.css"; // 여기에 CSS 파일 경로를 넣어야 해
 document.head.appendChild(linkElement); // 문서의 head에 link 요소를 추가하여 CSS를 가져옴
 
 const backend_base_url = config.backend_base_url
